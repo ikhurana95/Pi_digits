00e241cf779c995b6d080d6279473578f8ffaabf
femioladipo <oluwafemi.oladipo@kcl.ac.uk>
2018-02-18 19:19:56 +0000

    Camera function created.

--- camera/camera.py
+++ camera/camera.py
@@ -3,11 +3,29 @@
 
 from picamera import PiCamera
 from time import sleep
-
+import numpy as np
+from PIL import Image
 
 def shoot(index):
     camera = PiCamera()
     camera.start_preview()
     sleep(5)
-    camera.capture('/pics/pic' + index + '.jpg')
+    camera.capture('/pics/pic%s.jpg' % index)
     camera.stop_preview()
+
+'''
+img = Image.open('orig.png').convert('RGBA')
+arr = np.array(img)
+
+# record the original shape
+shape = arr.shape
+
+# make a 1-dimensional view of arr
+flat_arr = arr.ravel()
+
+# convert it to a matrix
+vector = np.matrix(flat_arr)
+'''
+
+
+shoot(1)
